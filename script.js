34aea836d5533dd6a64be9ac471aba1a4f498c9b
ghost211221 <solovyev.21@yandex.ru>
2019-04-07 21:02:24 +0300

    урок 4

--- script.js
+++ script.js
@@ -3,96 +3,106 @@
 ////////////////////////////////////////////
 /// 1
 ///////////////////////////////////////////
-var num       = 1;
-var simpleArr = [];
-var complex   = 0;
-console.log('Simple numbers:')
-while (num <= 100) {
-    var i     = 2;
-    complex   = 0;
-    while (i < num) {
-        if (num % i == 0) {
-            complex = 1;
-            // console.log(num, i, complex);
-            break;
+// var iNum   = +prompt('Введите число:', '');
 
-        }
-        i++;
-    }
-    if (complex == 0) {
-        simpleArr.push(num);
-        console.log('\t' + num);
-    }
-    num++;
-}
-console.log('Finished')
-console.log('==============================================')
+// var numObj = {
+//     hundr:  0,     // сотни
+//     dozen:  0,     // десятки
+//     units:  0,     // единцы
+//     convert(iNum) {
+//         if (iNum >= 1000) {
+//             console.log("Число слишком большое");
+//         } else {
+//             var str = "" + iNum;
+//             var arr = str.split('');
+//             console.log(str);
+//             // теперь в зависимости от количества знаков
+//             if (iNum < 10) {
+//                 this.units = iNum;
+//             } else if (iNum < 100) {
+//                 this.dozen = arr[0];
+//                 this.units = arr[1];
+//             } else {
+//                 this.hundr = arr[0];
+//                 this.dozen = arr[1];
+//                 this.units = arr[2];
+//             }
+//         }
+//     },
+//     reportNums() {
+//         console.log("Converted " + iNum + " to:");
+//         console.log("\tHundreeds " + this.hundr);
+//         console.log("\tDozens " + this.dozen);
+//         console.log("\tUnits " + this.units);
+//     }
+// };
+
+// numObj.convert(iNum);
+// numObj.reportNums();
+
 
 ////////////////////////////////////////////
-/// 2,3 
-/// корзину удобней реализовать как словарь
-/// basket[id] = {
-///     'prodName'  : name,     - название товара
-///     'prodCount' : count,    - количество
-///     'prodPrice' : price,    - цена за единицу
-///     'prodTotal' : total     - результирущая цена = count * price
-/// }
-///////////////////////////////////////////
-var basket = {};
-basket['prod1'] = {
-    'prodName'  : "ESP Horizon",
-    'prodCount' : 1,
-    'prodPrice' : 100000,
-    'prodTotal' : 0             // вычилим во время подсчета общей суммы
-};
-basket['prod2'] = {
-    'prodName'  : "ESP M2",
-    'prodCount' : 2,
-    'prodPrice' : 115000,
-    'prodTotal' : 0             // вычилим во время подсчета общей суммы
-};
-basket['prod3'] = {
-    'prodName'  : "ESP Arrow",
-    'prodCount' : 3,
-    'prodPrice' : 93000,
-    'prodTotal' : 0             // вычилим во время подсчета общей суммы
-};
-
-function countCurrentProd(prodId) {
-    basket[prodId]['prodTotal'] = basket[prodId]['prodCount'] * basket[prodId]['prodPrice'];
-}
-
-function countBasketPrice() {
-    var sumTotal = 0;
-    for (var prodId in basket){
-        countCurrentProd(prodId);
-        sumTotal += basket[prodId]['prodTotal'];
+/// 2
+////////////////////////////////////////////
+var basket = {
+    content: {
+        0: {
+            prodArt   : 0,              // артикул
+            prodName  : "ESP Horizon",
+            prodObj   : null,           // ссылка на объект продукта
+            prodCount : 1,
+            prodPrice : 100000,
+            prodTotal : 0               // вычилим во время подсчета общей суммы
+        },
+        1: {
+            prodArt   : 1,              // артикул
+            prodName  : "ESP M2",
+            prodObj   : null,           // ссылка на объект продукта
+            prodCount : 2,
+            prodPrice : 115000,
+            prodTotal : 0               // вычилим во время подсчета общей суммы
+        },
+        2: {
+            prodArt   : 2,              // артикул
+            prodName  : "ESP Arrow",
+            prodObj   : null,           // ссылка на объект продукта
+            prodCount : 3,
+            prodPrice : 93000,
+            prodTotal : 0               // вычилим во время подсчета общей суммы
+        },
+    },
+    sumTotal: 0,                        // общая стоимость корзины
+    countCurrentProd(prodId) {
+        this.content[prodId].prodTotal = this.content[prodId].prodCount * this.content[prodId].prodPrice;
+    },
+    countBasketPrice() {
+        var sumTotal = 0;
+        for (var prodId in basket.content){
+            // console.log(prodId);
+            this.countCurrentProd(prodId);
+            console.log(this.content[prodId].prodTotal);
+            this.sumTotal += this.content[prodId].prodTotal;
+        }
+        console.log('Total price: ' + this.sumTotal);
     }
-    console.log('Total price: ' + sumTotal);
-}
+};
 
 console.log('Counting basket:');
-countBasketPrice();
+basket.countBasketPrice();
 console.log('Finished');
 console.log('==============================================')
+
 ////////////////////////////////////////////
-/// 4
-///////////////////////////////////////////
-console.log('Use for to print 0-9');
-for (var i = 0; i < 10; console.log(i++)) {}
-console.log('Finished');
-console.log('==============================================')
+/// 3
 ////////////////////////////////////////////
-/// 5
-////////////////////////////////////////////
-for (var i = 0; i < 21; i++) {
-    var lineArr = [];
-    for (var j = 0; j < 21; j++) {
-        if (j < i) {
-            lineArr.push('x');
-        } else {
-            
-        }
-    }
-    console.log(lineArr.join(''));
-}
+//
+// Т.к. выбран магазин электрогитар,
+// сущность продукт должна содержать следующие поля:
+// 1 - артикул, уникальное поле
+// 2 - наименование
+// 3 - список ссылок на фотографии
+// 4 - количество струн
+// 5 - материал корпуса
+// 6 - количество звукоснимателей
+// ....
+// методы пока могу предположить только те, что были использованы в корзине
